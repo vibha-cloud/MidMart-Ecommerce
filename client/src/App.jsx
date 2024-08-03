f932278b85d7b10c01540dd8665421c65895f3bb
Vibha <vibhachandola23012005@gmail.com>
2024-08-03 15:13:26 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-slate-500">Hello, world!</h1>
+      <p>Welcome to the React app!</p>
+    </div>
+  );
+};
+
+export default App;
